13b2c1e278aaa508179bd54fa82839855cd4ba34
PabloQuipildor <pabloquipildor56@gmail.com>
2022-06-14 17:55:49 -0300

    creando nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div>
-      <h1>hola mundo</h1>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const NavBar = () => {
+  return (
+  <nav className="navbar navbar-expand-lg bg-light">
+    <div className="container-fluid">
+      <span className="navbar-brand" >LOGO
+        
+        <div>Nosotros</div>
+        <div>Mas Informacion</div>
+        </span>  
+    </div>
+  </nav>
+    )
+}
+
+export default NavBar
