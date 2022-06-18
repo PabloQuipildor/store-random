36a2c6856dafa16e36ae700b7eca09a5886ba2c9
PabloQuipildor <pabloquipildor56@gmail.com>
2022-06-18 17:33:08 -0300

    terminando Nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,19 @@
 import React from "react"
-
+import "./navBar.css";
 const NavBar = () => {
   return (
-  <nav className="navbar navbar-expand-lg bg-light">
-    <div className="container-fluid">
-      <a className="navbar-brand">Nav</a>
-      <button>hola</button>
-      <button>hola</button>
-      <button>hola</button>
-    </div>
+  <nav className="navbar navbar-expand-lg bg-dark justify-content-center">
+      <ul className="nav">
+        <li className="nav-item">
+          <a className="nav-link active"> BIOGRAFIA</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active">QUIENES SOMOS</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active">CONTACTOS</a>
+        </li>
+      </ul>
   </nav>
     )
 }
