34ace92525017f6f967942db6685468e484bfe1a
PabloQuipildor <pabloquipildor56@gmail.com>
2022-06-22 10:27:16 -0300

    agregando React Icons

---
+++ src/components/CartWidget.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+import "./cartWidget.css"
+
+const CartWidget = () => {
+  return (
+    <>
+      <AiOutlineShoppingCart className="carrito"/>
+    </>
+  )
+}
+export default CartWidget
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,24 @@
 import React from "react"
+import CartWidget from "./CartWidget";
 import "./navBar.css";
+
 const NavBar = () => {
   return (
   <nav className="navbar navbar-expand-lg bg-dark justify-content-center">
       <ul className="nav">
         <li className="nav-item">
-          <a className="nav-link active"> BIOGRAFIA</a>
+          <a className="nav-link "> BIOGRAFIA</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link active">QUIENES SOMOS</a>
+          <a className="nav-link ">QUIENES SOMOS</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link active">CONTACTOS</a>
+          <a className="nav-link ">CONTACTOS</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link">
+            <CartWidget/>
+          </a>
         </li>
       </ul>
   </nav>
